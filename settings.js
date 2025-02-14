52009ad1d45f223c2f7e6634f2bd9b795190f4e6
spic-rgb <chassupremo@gmail.com>
2025-02-14 21:37:35 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "PuByxB5D#PbqPVvJqvmx7uZl51wyy-UiFbIs9HqnAdXOP-leIffw", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 STATUES_REPLY: process.env.STATUES_REPLY || "true",
